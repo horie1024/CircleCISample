b7b609c020c061460c0ed35a218656659680f4d3
horie1024 <ryosuke.pic@gmail.com>
2015-01-14 18:17:41 +0900

    deploy.shを修正

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ./gradlew assembleRelease
 
-curl -F "file=@`pwd`/app/build/outputs/apk/app-release-unsigned.apk" -F "token=${DEPLOY_GATE_API_KEY}" -F "message=sample" https://deploygate.com/api/users/YOUR_USER_NAME/apps
+curl -F "file=@app/build/outputs/apk/app-release-unsigned.apk" -F "token=${DEPLOY_GATE_API_KEY}" -F "message=sample" https://deploygate.com/api/users/YOUR_USER_NAME/apps
